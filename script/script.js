2dcc511350decc9714181e79c0461c5632624a6e
mindwarp77 <mindwarp771@gmail.com>
2021-03-31 21:16:36 +0200

    Last final version

--- script/script.js
+++ script/script.js
@@ -30,11 +30,3 @@ setSuccessFor = (input) => {
     const formControl = input.parentElement;
     formControl.className = "form-control success";
 }
-
-validEmail = (email) => {
-    if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)) {
-        return true
-    } else {
-        return false
-    }
-}
